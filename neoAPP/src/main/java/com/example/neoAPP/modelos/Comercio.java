054fd4d39d0178607fc86d4f316a00d3804c66ac
maria.mejia <desarrollo@academicos.cesde.edu.co>
2026-02-16 11:30:39 -0500

    feat se crea capa de modelos

---
+++ neoAPP/src/main/java/com/example/neoAPP/modelos/Comercio.java
@@ -0,0 +1,7 @@
+package com.example.neoAPP.modelos;
+
+public class Comercio {
+
+    //id,nit,nombre,actividad,contacto(correo)
+
+}
---
+++ neoAPP/src/main/java/com/example/neoAPP/modelos/Usuario.java
@@ -0,0 +1,7 @@
+package com.example.neoAPP.modelos;
+
+public class Usuario {
+
+    //id,nombres,tipodocumento,documento,edad
+
+}
