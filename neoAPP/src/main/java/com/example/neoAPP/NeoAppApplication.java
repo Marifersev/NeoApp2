938b9cf1159556bdaf08c8d689b7f412445758da
maria.mejia <desarrollo@academicos.cesde.edu.co>
2026-02-16 09:44:56 -0500

    feat se crea proyecto

---
+++ neoAPP/src/main/java/com/example/neoAPP/NeoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.neoAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoAppApplication.class, args);
+	}
+
+}
