53513eb9cf599b515eaaada76568a3ce19e6e4b3
maria.mejia <desarrollo@academicos.cesde.edu.co>
2026-03-09 09:18:05 -0500

    feat se crea base

--- neoAPP/src/main/java/com/example/neoAPP/modelos/Categoria.java
+++ neoAPP/src/main/java/com/example/neoAPP/modelos/Categoria.java
@@ -2,6 +2,7 @@ package com.example.neoAPP.modelos;
 
 public class Categoria {
 
-    //id, nombre, fechaCreacion, responsable, justificacion
+    
+    
 
 }
--- neoAPP/src/main/java/com/example/neoAPP/modelos/Usuario.java
+++ neoAPP/src/main/java/com/example/neoAPP/modelos/Usuario.java
@@ -1,7 +1,62 @@
 package com.example.neoAPP.modelos;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Usuario {
 
-    //id,nombres,tipodocumento,documento,edad
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombres;
+    private String tipoDocumento;
+    private String documento;
+    private Integer edad;
+
+     public Usuario() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
 
 }
